88a53c78969947c90d70381bff184868328bb9f4
Jaspreet Singh <jaspreet237@gmail.com>
2025-08-07 09:08:14 +0530

    Provision nginx using Terraform and Docker

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.2"
+    }
+  }
+}
+
+provider "docker" {
+  host = "npipe:////.//pipe//docker_engine" # For Windows
+  # For Linux/Mac: use `unix:///var/run/docker.sock`
+}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx_server" {
+  name  = "nginx-container"
+  image = docker_image.nginx.name
+  ports {
+    internal = 80
+    external = 8081
+  }
+}
